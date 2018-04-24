7b17d433323818ce6c7a2aab9e4cdc21fe46fcf3
silence <35656692+silence48@users.noreply.github.com>
2018-04-24 18:01:50 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1607,26 +1607,26 @@ int64_t GetBlockValue(int nHeight)
 // in Fantasy Gold, we have referred to this as the SubsidyDecrease
 {
 	int64_t nSubsidy = 0;
-	int nSubsidyDecreasePeriod, nSubsidyPeriod, i;
-	float nSubsidyDecreasePercent;
-	nSubsidyDecreasePercent = .125; //Subsidy Decrease Percentage Per Period
-	nSubsidyDecreasePeriod = 86400; //Number of Blocks in the desired time period for the reward to decrease.
-	//this could also be obtained by using the desired time period in days (90) * 24 hours * 60 minutes * 60 seconds ) / blocktime (90) = 86,400
+	int64_t nSubsidyDecreasePeriod = 86400, nSubsidyPeriod, nSubsidyDecreasePercent = .125, nSubsidyIteration;
+	// nSubsidyDecreasePercent = .125; // initialized above Subsidy Decrease Percentage Per Period
+	// nSubsidyDecreasePeriod = 86400; // initialized above Number of Blocks in the desired time period for the reward to decrease.
+	/*//this could also be obtained by using the desired time period in days (90) * 24 hours * 60 minutes * 60 seconds ) / blocktime (90) = 86,400
 	//in fact maybe we should initialize the variables in chainparams... using something like the following:
 	// Params().nRewardDecrease.days Params().nTargetSpacing Params().nRewardDecrease.percentperperiod 
 	// nDecreasePeriod = (Params().nRewardDecrease.Days * 24 * 60 * 60) / Params().nTargetSpacing
 	// in this codebase this is unnecessary because our decrease period is 90 days and our block spacing is 90 days thus 86400 is the amount of blocks in days
-	// as well as the amount of blocks in seconds.
-	nSubsidyPeriod = floor(nHeight/nSubsidyDecreasePeriod); //This calculates which period of subsidy decrease the current block is in, if this is 0 then the reward is 50
+	// as well as the amount of blocks in seconds.*/
+	nSubsidyPeriod = floor (nHeight/nSubsidyDecreasePeriod); //This calculates which period of subsidy decrease the current block is in, if this is 0 then the reward is 50
 	if (Params().NetworkID() == CBaseChainParams::TESTNET) {
-		int nDecreasePeriodTestnet, nSubsidyPeriodTestnet, i;
+		nSubsidy = 50 * COIN;
+		int64_t nDecreasePeriodTestnet, nSubsidyPeriodTestnet;
 		nDecreasePeriodTestnet = 100;
 		nSubsidyPeriodTestnet = floor(nHeight/nDecreasePeriodTestnet);
-		i = 0;
+		nSubsidyIteration = 0;
 		while (nSubsidyPeriodTestnet >= 1) {
-			i++;
+			nSubsidyIteration++;
 			nSubsidy = (nSubsidy - (nSubsidyDecreasePercent * nSubsidy) * COIN);
-			if ( i == nSubsidyPeriodTestnet) {
+			if ( nSubsidyIteration == nSubsidyPeriodTestnet) {
 				break;
 			}
 		} 
@@ -1634,37 +1634,37 @@ int64_t GetBlockValue(int nHeight)
 	if (nHeight == 0) {
 		nSubsidy = 4750000 * COIN;
 	}
-	else if (nSubsidyDecreasePeriod < 1) {
+	else if (nHeight > 1 && nHeight < 86400) {
 		nSubsidy = 50 * COIN;
 	}
 	else {
-		i = 0;
+		nSubsidyIteration = 0;
 		while (nSubsidyPeriod >= 1) {
-            i++;
+            nSubsidyIteration++;
 			nSubsidy = (nSubsidy - (nSubsidyDecreasePercent * nSubsidy) * COIN);
-            if ( i == nSubsidyPeriod) {
+            if ( nSubsidyIteration == nSubsidyPeriod) {
 				break;
 			}
 		}
 	}
-		return nSubsidy;
-	}
-	
+	return nSubsidy;
+}
+
 int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCount)
 {
     int64_t ret = 0;
     
     if (Params().NetworkID() == CBaseChainParams::TESTNET) {
 		if (nHeight < 200) {
-        return 0;
+			return 0;
 		}
 	}
     if (nHeight < Params().LAST_POW_BLOCK() ) {
         return 0;
     }
-    if (nHeight <= 86400) { 
+    if (nHeight >= Params()LAST_POW_BLOCK() ) { 
         ret = blockValue * .8; 
-        }
+	}
     
 
     return ret;
