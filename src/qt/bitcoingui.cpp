50f9a702647f363646d3377665df40f9103dd495
Silence <Silence@FantasyGold.io>
2019-10-11 01:50:49 +0100

    gui: Make RPCConsole::TabTypes an enum class

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -922,7 +922,7 @@ void BitcoinGUI::showDebugWindow()
 
 void BitcoinGUI::showDebugWindowActivateConsole()
 {
-    rpcConsole->setTabFocus(RPCConsole::TAB_CONSOLE);
+    rpcConsole->setTabFocus(RPCConsole::TabTypes::CONSOLE);
     showDebugWindow();
 }
 
