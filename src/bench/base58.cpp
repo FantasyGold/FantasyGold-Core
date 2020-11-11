cd5aff5d7fd4891c60e15f9684c77700383e799a
silence <35656692+silence48@users.noreply.github.com>
2020-11-10 21:13:39 -0500

    update bench files

--- src/bench/base58.cpp
+++ src/bench/base58.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2018 The Bitcoin Core developers
+// Copyright (c) 2016-2019 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -8,7 +8,6 @@
 
 #include <array>
 #include <vector>
-#include <string>
 
 
 static void Base58Encode(benchmark::State& state)
