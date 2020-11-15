4f7ede4864de98ea7cadc376c7e5d01f477ce908
silence <35656692+silence48@users.noreply.github.com>
2020-11-15 13:57:30 -0500

    update policy

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -76,7 +76,6 @@ static constexpr unsigned int STANDARD_NOT_MANDATORY_VERIFY_FLAGS = STANDARD_SCR
 /** Used as the flags parameter to sequence and nLocktime checks in non-consensus code. */
 static constexpr unsigned int STANDARD_LOCKTIME_VERIFY_FLAGS = LOCKTIME_VERIFY_SEQUENCE |
                                                                LOCKTIME_MEDIAN_TIME_PAST;
-
 /** The number of sender stack items in a standard sender signature script */
 static const unsigned int STANDARD_SENDER_STACK_ITEMS = 2;
 /** The maximum size of each sender stack item in a standard sender signature script */
